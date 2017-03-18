8c81d7dd075f423c166b8ddaa8d88d4ffa27c71f
James Turnbull <james@lovedthanlost.net>
2017-03-17 22:23:47 -0400

    Added remote state

--- production/main.tf
+++ production/main.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = "${var.region}"
 }
 
+terraform {
+  backend "s3" {
+    region = "us-east-1"
+    bucket = "examplecom-remote_state-production"
+    key    = "terraform.tfstate"
+  }
+}
+
 module "vpc" {
   source          = "git@github.com:turnbullpublishing/tf_vpc.git?ref=v0.0.2"
   environment     = "${var.environment}"
